108019ab0d7127d18b670bff08faa920fe80ae23
Tharin <tharin2501@hotmail.no>
2020-10-22 20:14:19 +0200

    transistion

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import city2 from "./images/city2.png";
 
 function App() {
     return (
-        <div className="container-fluid"
+        <div className="container-fluid fade-inn-transition"
              style={{background: `url(${city2})`, minWidth: "100wv", minHeight: "100vh", backgroundSize: "cover"}}>
             <CryptoCompare/>
         </div>
